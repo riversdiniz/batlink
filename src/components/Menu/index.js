e5362c3bf862c40d09e991a9436dd7a74420f2c2
River Diniz <river-s-diniz@hotmail.com>
2022-02-02 00:05:17 -0400

    Finalizando as duas páginas.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,21 @@
 import './menu.css';
+import { BsYoutube, BsInstagram } from 'react-icons/bs';
+import { Link } from 'react-router-dom'
 
 export default function Menu(){
     return(
-        <div>
-            <h1>MENU PAGINA</h1>
+        <div className='menu'>
+            <a className="social" href="https://www.youtube.com/channel/UCLc5Bq2yfs-S3Zse3ZFRMEQ">
+                <BsYoutube color="#FFF" size={24}/>
+            </a>
+
+            <a className="social" href="https://www.instagram.com/riversdiniz/">
+                <BsInstagram color="#FFF" size={24}/>
+            </a>
+            <Link className="menu-item" to="/links">
+                Meus Links
+            </Link>
+            
         </div>
     )
 }
