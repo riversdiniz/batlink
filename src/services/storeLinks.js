6ac8c159495d519c9c0fd917b638319594e037b2
River Diniz <river-s-diniz@hotmail.com>
2022-02-03 16:43:07 -0400

    Criando o novo arquivo storedLinks.js

---
+++ src/services/storeLinks.js
@@ -0,0 +1,30 @@
+// Buscar os links salvos.
+
+export async function getLinksSave(key){
+    const myLinks = await localStorage.getItem(key)
+
+    let linksSaves = JSON.parse(myLinks) || [];
+
+    return linksSaves;
+
+}
+
+// Salvar um link no localstorage.
+export async function saveLink(key, newLink){
+    let linksStored = await getLinksSave(key);
+
+    // Se já tiver um link salvo com algum ID eu não vou deixar duplicar.
+    const hasLink = linksStored.some( link => link.id === newLink.id)
+
+    if(hasLink){
+        console.log('ESSE LINK JÁ EXISTE NA LISTA!')
+        return;
+    }
+
+    // Adicionar esse novo link na lista...
+    linksStored.push(newLink)
+    await localStorage.setItem(key, JSON.stringify(linksStored))
+    console.log('LINK SALVO COM SUCESSO!')
+}
+
+// Deletar algum link salvo.
